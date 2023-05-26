fc9f118fa192cc84a035d4879b3946cb9336fdc5
Victor Hugo Silva <102427408+victorhugosilvablancas@users.noreply.github.com>
2023-05-25 18:34:19 -0600

    mlp_datosbrutosdata.java

--- mlp_datosbrutosdata.java
+++ mlp_datosbrutosdata.java
@@ -16,11 +16,6 @@ import java.util.List;
  * @author Victor Hugo Silva
  */
 public class mlp_datosbrutosdata {
-    private static final String adriver="oracle.jdbc.OracleDriver";
-    private static final String aurl="jdbc:oracle:thin:@DATAWAREHOUSE:1521:XE";
-    private static final String ausuario="MLP_USR";
-    private static final String aclave="aBc1D23";
-
     private static final String ARCHIVO="mlp_datosbrutos.csv";
     private static final String COMMA=",";
     private static String[] Tablas=new String[] {
@@ -106,56 +101,7 @@ public class mlp_datosbrutosdata {
      * 1.extraer datos brutos a: mlp_datosbrutos.csv
      */
     public void Extraer_datos_brutos() {
-        System.out.println("1.Extraer datos brutos");
-        try {
-            File f=new File(ARCHIVO);
-            FileWriter fw=new FileWriter(f);
-            PrintWriter pw=new PrintWriter(fw);
-            pw.println(mlp_datosbrutosdata.CABEZA);
-            pw.println(mlp_datosbrutosdata.TIPOS);
-            
-            Class.forName(adriver);
-            Connection conn = DriverManager.getConnection(aurl,UNKNOWN,UNKNOWN); 
-            Statement stmt = conn.createStatement();
-            ResultSet rs;
-            String atabla;
-            String instruccion;
-            int contador=0;
-            mlp_datosbrutosdata midato=new mlp_datosbrutosdata();
-            for (int iperiodo=2012;iperiodo<2023;iperiodo++) {
-                for (int itabla=0;itabla<Tablas.length;itabla++) {
-                    atabla=Tablas[itabla]+iperiodo;
-                    instruccion="select table_name from user_tables where table_name='"+atabla+"' ";
-                    Boolean haytabla=false;
-                    rs=stmt.executeQuery(instruccion);
-                    while (rs.next()) {
-                        haytabla=rs.getString(1).length()>0;
-                    }
-                    if (haytabla) {
-                        instruccion="select idmov,idproducto,cantidad from "+atabla+" ";
-                        rs=stmt.executeQuery(instruccion);
-                        while (rs.next()) {
-                            midato=new mlp_datosbrutosdata();
-                            midato.periodo=iperiodo;
-                            midato.tabla=atabla;
-                            midato.idmov=rs.getInt(1);
-                            midato.idproducto=rs.getInt(2);
-                            midato.cantidad=rs.getDouble(3);
-
-                            pw.println(midato.cadena());
-
-                            contador++;
-                            System.out.println(iperiodo+":"+contador);
-                        }
-                    }
-                }
-            }
-            pw.close();
-            fw.close();
-            stmt.close();conn.close();
-        } catch (Exception e) {
-            System.out.println("1.Extraer_datos_brutos: "+e.getLocalizedMessage());
-        }
+        //code
     }
     /**
      * Instrucción para crear el esquema para los datos en la instancia de SQL.
@@ -191,166 +137,22 @@ public class mlp_datosbrutosdata {
      * 3.crear tabla recolectora
      */
     public void Crear_tabla_recolectora() {
-        try {
-            Class.forName(adriver);
-            Connection conn = DriverManager.getConnection(aurl,ausuario,aclave); 
-            Statement stmt = conn.createStatement();
-            
-            System.out.println("3.Crear tabla recolectora");
-            String instruccion;
-            ResultSet rs;
-            
-            instruccion="select table_name from user_tables where table_name='"+mlp_datosbrutosdata.TABLA+"' ";
-            Boolean haytabla=false;
-            rs=stmt.executeQuery(instruccion);
-            while (rs.next()) {
-                haytabla=rs.getString(1).length()>0;
-            }
-            if (!haytabla) {
-                stmt.executeUpdate(mlp_datosbrutosdata.CREA_TABLA);
-                System.out.println("tabla creada");
-            } else {
-                stmt.executeUpdate(mlp_datosbrutosdata.LIMPIA_TABLA);
-                System.out.println("tabla limpiada");
-            }
-            stmt.close();conn.close();
-        } catch (Exception e) {
-            System.out.println("3.Crear_tabla_recolectora: "+e.getLocalizedMessage());
-        }
+        //code
     }
     /**
      * 4.leer datos brutos de csv
      */
     public void Leer_datos_brutos() {
-        Timestamp fini=new Timestamp(System.currentTimeMillis());
-        listadatos=new ArrayList<>();
-        try {
-            System.out.println("3. Leer datos brutos");
-            
-            BufferedReader br = new BufferedReader(new FileReader(ARCHIVO));
-            String line = br.readLine();
-            int irenglon=0;
-            
-            mlp_datosbrutosdata midato=new mlp_datosbrutosdata();
-            while (line != null) {
-                if (irenglon>1) {
-                    midato=new mlp_datosbrutosdata(line);
-                    listadatos.add(midato);
-                }
-                line = br.readLine();
-                irenglon++;
-                System.out.print(".");
-                if (irenglon % 100 == 0) System.out.println();
-            }
-            br.close();
-            System.out.println("FIN leer datos brutos "+listadatos.size());
-        } catch (Exception e) {
-            System.out.println("4.Leer_datos_brutos: "+e.getLocalizedMessage());
-        }
-        Timestamp ffin=new Timestamp(System.currentTimeMillis());
-
-        Long tiempoini=fini.getTime();
-        Long tiempofin=ffin.getTime();
-        Integer tiempo=tiempofin.intValue()-tiempoini.intValue();
-        System.out.println("Resultado: "+tiempo/1000+" segundos");
+        //code
     }
     /**
      * 5.llenar tabla recolectora
      */
     public void Llenar_tabla_recolectora() {
-        Timestamp fini=new Timestamp(System.currentTimeMillis());
-        try {
-            System.out.println("5.Llenar tabla recolectora");
-            
-            Class.forName(adriver);
-            Connection conn = DriverManager.getConnection(aurl,ausuario,aclave); 
-            Statement stmt = conn.createStatement();
-            
-            mlp_datosbrutosdata midato=new mlp_datosbrutosdata();
-            for (int i=0;i<listadatos.size();i++) {
-                midato=listadatos.get(i);
-                stmt.executeUpdate(midato.inserta());
-                System.out.print(".");
-                if (i % 100 == 0) System.out.println();
-            }
-            System.out.println("FIN llenar tabla recolectora");
-            stmt.close();conn.close();
-        } catch (Exception e) {
-            System.out.println("5.Llenar tabla recolectora: "+e.getLocalizedMessage());
-        }
-        Timestamp ffin=new Timestamp(System.currentTimeMillis());
-
-        Long tiempoini=fini.getTime();
-        Long tiempofin=ffin.getTime();
-        Integer tiempo=tiempofin.intValue()-tiempoini.intValue();
-        System.out.println("Resultado: "+tiempo/1000/60+" minutos");
+        //code
     }
     public void Consultar_con_PLSQL() {
-        try {
-            Class.forName(adriver);
-            Connection conn = DriverManager.getConnection(aurl,ausuario,aclave); 
-            Statement stmt = conn.createStatement();
-
-            System.out.println("6.Consultar con pl/sql (medir tiempo)");
-            
-            mlp_datosbrutosdata midato=new mlp_datosbrutosdata();
-            
-            Timestamp fini=new Timestamp(System.currentTimeMillis());
-            System.out.println("Inicio: "+fini.toString());
-            //1. Consulta a punto de línea
-            String instruccion="select * from "+mlp_datosbrutosdata.TABLA+" ";
-            ResultSet rs=stmt.executeQuery(instruccion);
-            while (rs.next()) {
-                midato=new mlp_datosbrutosdata();
-                midato.periodo=rs.getInt(1);
-                midato.tabla=rs.getString(2);
-                midato.idmov=rs.getInt(3);
-                midato.idproducto=rs.getInt(4);
-                midato.cantidad=rs.getDouble(5);
-            }
-            Timestamp ffin=new Timestamp(System.currentTimeMillis());
-            
-            Long tiempoini=fini.getTime();
-            Long tiempofin=ffin.getTime();
-            Integer tiempo=tiempofin.intValue()-tiempoini.intValue();
-            System.out.println("Resultado a punto: "+tiempo+" milésimas de segundo");
-            
-            //2. Consulta a grupo
-            fini=new Timestamp(System.currentTimeMillis());
-            instruccion="select periodo,count(*) from "+mlp_datosbrutosdata.TABLA+" group by periodo ";
-            rs=stmt.executeQuery(instruccion);
-            while (rs.next()) {
-                midato=new mlp_datosbrutosdata();
-                midato.periodo=rs.getInt(1);
-                midato.cuantos=rs.getInt(2);
-            }
-            ffin=new Timestamp(System.currentTimeMillis());
-            
-            tiempoini=fini.getTime();
-            tiempofin=ffin.getTime();
-            tiempo=tiempofin.intValue()-tiempoini.intValue();
-            System.out.println("Resultado a grupo: "+tiempo+" milésimas de segundo");
-            
-            //3. Consulta total
-            fini=new Timestamp(System.currentTimeMillis());
-            instruccion="select count(*) from "+mlp_datosbrutosdata.TABLA+" ";
-            rs=stmt.executeQuery(instruccion);
-            while (rs.next()) {
-                midato=new mlp_datosbrutosdata();
-                midato.cuantos=rs.getInt(1);
-                System.out.println(midato.cuantos);
-            }
-            ffin=new Timestamp(System.currentTimeMillis());
-            
-            tiempoini=fini.getTime();
-            tiempofin=ffin.getTime();
-            tiempo=tiempofin.intValue()-tiempoini.intValue();
-            System.out.println("Resultado total: "+tiempo+" milésimas de segundo");
-            
-            stmt.close();conn.close();
-        } catch (Exception e) {
-            System.out.println("2,3,4 y 5. Leer datos brutos y crear tabla recolectora: "+e.getLocalizedMessage());
-        }
+        //code
     }
     public void Todos() {
         Extraer_datos_brutos();
